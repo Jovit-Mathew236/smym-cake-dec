6da63f884f594a66e5b30a7d83a7605fd97ec058
Jovit Mathew <70875875+Jovit-Mathew236@users.noreply.github.com>
2022-02-16 23:11:39 +0530

    Update Config.js

--- src/firebase/Config.js
+++ src/firebase/Config.js
@@ -3,12 +3,13 @@ import firebase from 'firebase'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBqzx0Hfnq7FIA0z_31WBxGbj5Q49gBM9Q",
-  authDomain: "smymcakepro.firebaseapp.com",
-  projectId: "smymcakepro",
-  storageBucket: "smymcakepro.appspot.com",
-  messagingSenderId: "863486378318",
-  appId: "1:863486378318:web:a670d3328bff428e16de1f"
+  apiKey: "AIzaSyBNwWZwxMOSE2g25FFOxMb0-XhWqE6-NaA",
+  authDomain: "smym-cake-exd.firebaseapp.com",
+  projectId: "smym-cake-exd",
+  storageBucket: "smym-cake-exd.appspot.com",
+  messagingSenderId: "848347487606",
+  appId: "1:848347487606:web:39aa921b02a4817b2e2e57",
+  measurementId: "G-DFFTKHEZY3"
 };
 
 export default firebase.initializeApp(firebaseConfig)
