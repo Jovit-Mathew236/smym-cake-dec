f0639fd256032df4e5dea4b244427a8f84c051ad
Jovit Mathew <70875875+Jovit-Mathew236@users.noreply.github.com>
2022-02-16 23:23:50 +0530

    Update Config.js

--- src/firebase/Config.js
+++ src/firebase/Config.js
@@ -2,13 +2,14 @@ import firebase from 'firebase'
 
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBqzx0Hfnq7FIA0z_31WBxGbj5Q49gBM9Q",
-  authDomain: "smymcakepro.firebaseapp.com",
-  projectId: "smymcakepro",
-  storageBucket: "smymcakepro.appspot.com",
-  messagingSenderId: "863486378318",
-  appId: "1:863486378318:web:a670d3328bff428e16de1f"
+  apiKey: "AIzaSyCQb3jLpMbMrN4mgSdNAmTk7QfaEF89ONg",
+  authDomain: "smym-cake-order-8c5fe.firebaseapp.com",
+  projectId: "smym-cake-order-8c5fe",
+  storageBucket: "smym-cake-order-8c5fe.appspot.com",
+  messagingSenderId: "704106586693",
+  appId: "1:704106586693:web:d31257cbdb84f19ba9392c",
+  measurementId: "G-RN08FMNMF0"
 };
-
 export default firebase.initializeApp(firebaseConfig)
