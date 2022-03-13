553e1201ffea72a81ddbd4d01469f5631c0b6957
Jovit Mathew <70875875+Jovit-Mathew236@users.noreply.github.com>
2022-03-14 00:39:33 +0530

    Update Config.js

--- src/firebase/Config.js
+++ src/firebase/Config.js
@@ -4,12 +4,22 @@ import firebase from 'firebase'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCQb3jLpMbMrN4mgSdNAmTk7QfaEF89ONg",
-  authDomain: "smym-cake-order-8c5fe.firebaseapp.com",
-  projectId: "smym-cake-order-8c5fe",
-  storageBucket: "smym-cake-order-8c5fe.appspot.com",
-  messagingSenderId: "704106586693",
-  appId: "1:704106586693:web:d31257cbdb84f19ba9392c",
-  measurementId: "G-RN08FMNMF0"
+  apiKey: "AIzaSyBNwWZwxMOSE2g25FFOxMb0-XhWqE6-NaA",
+  authDomain: "smym-cake-exd.firebaseapp.com",
+  projectId: "smym-cake-exd",
+  storageBucket: "smym-cake-exd.appspot.com",
+  messagingSenderId: "848347487606",
+  appId: "1:848347487606:web:39aa921b02a4817b2e2e57",
+  measurementId: "G-DFFTKHEZY3"
 };
+
 export default firebase.initializeApp(firebaseConfig)
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBqzx0Hfnq7FIA0z_31WBxGbj5Q49gBM9Q",
+//   authDomain: "smymcakepro.firebaseapp.com",
+//   projectId: "smymcakepro",
+//   storageBucket: "smymcakepro.appspot.com",
+//   messagingSenderId: "863486378318",
+//   appId: "1:863486378318:web:a670d3328bff428e16de1f"
+// };
